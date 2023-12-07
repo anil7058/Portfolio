4e51f93bd3dd1bec93568485ff7af3b1fdda5eed
Akshay Gehlot <99969575+anil7058@users.noreply.github.com>
2023-12-07 06:56:37 -0500

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -75,7 +75,7 @@ const Footer = () => {
         <Row className="mt-5">
           <Col lg="12" md="12">
             <p className="copyright__text">
-            © 2021 {new Date().getFullYear()}., Creacted by<a href="https://www.instagram.com/akshay.k.gehlot/" title="akshay"></a> Akshay Gehlot. All Rights
+            © 2021 {new Date().getFullYear()}, Creacted by<a href="https://www.instagram.com/akshay.k.gehlot/" title="akshay"></a> Akshay Gehlot. All Rights
               Reserved.   
             </p>
           </Col>
